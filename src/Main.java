e440e36674fd9468bd4790e603cf682d530f7a4c
Bkarybaev <baielkarybaev@gmail.com>
2024-11-11 20:21:05 +0600

    start commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Country country1 = new Country("Baiel",28,"kyrgyz");
-        Country country2 = new Country("Baiel",29,"kyrgyz");
-        Country country3 = new Country("Baiel",38,"kyrgyz");
+        Country country1 = new Country("Kyrgyzstan",198,"kyrgyz");
+        Country country2 = new Country("Russia",29,"rushian");
+        Country country3 = new Country("China",38,"Chainase");
        Country[] array = Country.countries = new Country[]{country1,country2,country3};
 
         System.out.println(Country.setCountries(array));
