b69abe01e5d247b536701b3d023660daa72f4e30
Bkarybaev <baielkarybaev@gmail.com>
2024-11-11 20:19:04 +0600

    first commit

--- src/Country.java
+++ src/Country.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
 public class Country {
+    private String name;
+    private int aianty;
+    private String languahje;
+    static Country[] countries;
+
+    public Country() {
+
+    }
+
+    public Country(String name, int aianty, String languahje) {
+        this.name = name;
+        this.aianty = aianty;
+        this.languahje = languahje;
+    }
+
+
+    public int getAianty() {
+        return aianty;
+    }
+
+    public static Country setCountries(Country[] countries) {
+        Country array =  countries[0];
+        for (Country country : countries) {
+            if (country.getAianty() > array.getAianty()){
+                array = country;
+            }
+        }
+        return array;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                ", aianty=" + aianty +
+                ", languahje='" + languahje + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Country country1 = new Country("Baiel",28,"kyrgyz");
+        Country country2 = new Country("Baiel",29,"kyrgyz");
+        Country country3 = new Country("Baiel",38,"kyrgyz");
+       Country[] array = Country.countries = new Country[]{country1,country2,country3};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(Country.setCountries(array));
+
     }
 }
